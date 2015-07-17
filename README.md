939c830c313d2e798f384c066e52eee038d0e812
ajiyoshi-vg <Yoichi_Sudo@voyagegroup.com>
2015-07-17 14:29:59 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+MemcacheDB::Dump - It's new $module
+
+# SYNOPSIS
+
+    use MemcacheDB::Dump;
+
+# DESCRIPTION
+
+MemcacheDB::Dump is ...
+
+# LICENSE
+
+Copyright (C) ajiyoshi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+ajiyoshi <yoichi@ajiyoshi.org>
---
+++ lib/MemcacheDB/Dump.pm
@@ -0,0 +1,39 @@
+package MemcacheDB::Dump;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+MemcacheDB::Dump - It's new $module
+
+=head1 SYNOPSIS
+
+    use MemcacheDB::Dump;
+
+=head1 DESCRIPTION
+
+MemcacheDB::Dump is ...
+
+=head1 LICENSE
+
+Copyright (C) ajiyoshi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+ajiyoshi E<lt>yoichi@ajiyoshi.orgE<gt>
+
+=cut
+
